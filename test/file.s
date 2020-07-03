e606b441baef28f3ac42902577544dc3666535da
FlatGlobus <wtlbuilder@gmail.com>
2020-07-03 11:22:20 +0300

    fix typo

--- test/file.s
+++ test/file.s
@@ -1,13 +1,13 @@
 var temp_dir = temp_directory_path();
 print("temp dir: " + temp_dir);
 
-temp_dir /= "txte_text";
+temp_dir /= "txte_test";
 print("Create directory : " + temp_dir);
 create_directory(temp_dir);
 
 if(exists(temp_dir))
 {
-	print("Directory " + temp_dir + " createt.");
+	print("Directory " + temp_dir + " created.");
 
 	print("Remove " + temp_dir + " directory.");
 	remove(temp_dir);
